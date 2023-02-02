a735e637f7d8975f7443987260135df82a97df9a
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-02 13:36:40 +0200

    update

--- 003/index.js
+++ 003/index.js
@@ -10,9 +10,51 @@ Array.prototype.go = function (fun) {
     }
 };
 
-B.push('d');
+const animals = ['racoon', 'beaver', 'moose', 'fox'];
+
+B.push('');
 
 console.log(A, B);
 
 B.go(addStars);
 B.forEach(addStars);
+
+console.log(A);
+
+const forEachReturn = A.forEach((l) => console.log(l));
+
+const mapReturn = A.map((l) => l + '--->');
+
+console.log(forEachReturn);
+console.log(mapReturn);
+
+console.clear();
+
+// REACT PROGRAMER
+
+const out = animals.map((a) => '<i style="color: crimson; display:block;">' + a + '</i>');
+
+// REACT INSIDE
+
+let HTML = '';
+out.forEach((a) => (HTML += a));
+
+document.querySelector('h1').innerHTML = HTML;
+
+console.clear();
+
+// Filter method
+
+const filtered = animals.filter((l) => l != 'beaver').map((l) => l + ' * ');
+console.log(animals);
+console.log(filtered);
+
+console.clear();
+
+const state = ['racoon', 'beaver', 'moose', 'fox'];
+
+// state.push('wolf'); // mirstam
+// state.sort(); // mirstam
+
+console.log(state);
+console.log([...state, 'wolf'].sort());
