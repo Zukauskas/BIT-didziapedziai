bcaeb11c56ee8380d4071baa117bab7faa4b06a8
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-07 22:08:28 +0200

    Classes 4th task

--- homework/005/Classes/index.js
+++ homework/005/Classes/index.js
@@ -36,7 +36,7 @@ kibiriukas.kiekPririnktaAkmenu();
 
 /* 
 
-Sukurti klasę Pinigine. Konstruktoriuje sukurti dvi savybes popieriniaiPinigai ir metaliniaiPinigai. Parašyti metodą ideti(kiekis), kuris prideda pinigus į piniginę. Jeigu kiekis nedidesnis už 2, tai prideda prie metaliniaiPinigai, jeigu kitaip- prie popieriniaiPinigai. Parašykite metodą skaiciuoti(), kuris suskaičiuotų ir išvestų į konsolę popieriniaiPinigai ir metaliniaiPinigai sumą. Sukurti klasės objektą ir pademonstruoti veikimą. Nesvarbu kokios popierinės kupiūros ir metalinės monetos egzistuoja realiame pasaulyje.
+2. Sukurti klasę Pinigine. Konstruktoriuje sukurti dvi savybes popieriniaiPinigai ir metaliniaiPinigai. Parašyti metodą ideti(kiekis), kuris prideda pinigus į piniginę. Jeigu kiekis nedidesnis už 2, tai prideda prie metaliniaiPinigai, jeigu kitaip- prie popieriniaiPinigai. Parašykite metodą skaiciuoti(), kuris suskaičiuotų ir išvestų į konsolę popieriniaiPinigai ir metaliniaiPinigai sumą. Sukurti klasės objektą ir pademonstruoti veikimą. Nesvarbu kokios popierinės kupiūros ir metalinės monetos egzistuoja realiame pasaulyje.
 
 */
 
@@ -72,7 +72,12 @@ pinigine.skaiciuoti();
 console.clear();
 
 /*
- Sukurti klasę Troleibusas. Konstruktoriuje sukurti savybę keleiviuSkaicius kuri yra lygi 0. Parašyti du metodus: ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius). O taip pat parašyti metoda vaziuoja(), kuris į konsolę išvestų troleibusu važiuojančių keleivių skaičių. Atkreipkite dėmesį, kad troleibusu važiuoti neigiamas keleivių skaičius negali.
+ 3. Sukurti klasę Troleibusas. Konstruktoriuje sukurti savybę keleiviuSkaicius kuri yra lygi 0. Parašyti du metodus: ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius). O taip pat parašyti metoda vaziuoja(), kuris į konsolę išvestų troleibusu važiuojančių keleivių skaičių. Atkreipkite dėmesį, kad troleibusu važiuoti neigiamas keleivių skaičius negali.
+*/
+
+/* 
+4. (STATIC) Sukurti metodą keleiviuSkaiciusVisuoseTroleibusuose(), kuris rodytų bendrą keleivių skaičių visuose Troleibusas objektuose. Bendram kelevių skaičiaus skaičiavimui sukurkite statinį metodą bendrasKeleiviuSkaicius(keleiviuSkaicius), kuris pridėtų arba atimtų keleivius iš statinės savybės visiKeleiviai (kurioje yra įrašytas bendras keleivių skaičius). Taip pat atitinkamai modifikuokite metodus ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius).
+
 */
 
 class Troleibusas {
@@ -80,8 +85,19 @@ class Troleibusas {
         this.keleiviuSkaicius = 0;
     }
 
+    static visiKeleiviai = 0;
+
+    static bendrasKeleiviuSkaicius(keleiviuSkaicius) {
+        this.visiKeleiviai += keleiviuSkaicius;
+    }
+
+    static keleiviuSkaiciusVisuoseTroleibusuose() {
+        console.log(Troleibusas.visiKeleiviai);
+    }
+
     ilipa(keleiviai) {
         this.keleiviuSkaicius += keleiviai;
+        Troleibusas.bendrasKeleiviuSkaicius(keleiviai);
     }
 
     islipa(keleiviai) {
@@ -90,6 +106,7 @@ class Troleibusas {
             return;
         }
         this.keleiviuSkaicius -= keleiviai;
+        Troleibusas.bendrasKeleiviuSkaicius(-keleiviai);
     }
 
     vaziuoja() {
@@ -97,20 +114,23 @@ class Troleibusas {
     }
 }
 
-const troleibusas = new Troleibusas();
+const troleibusas1 = new Troleibusas();
+const troleibusas2 = new Troleibusas();
 
-troleibusas.ilipa(10);
+troleibusas1.ilipa(5);
 
-troleibusas.vaziuoja();
+troleibusas1.vaziuoja(); // 5
 
-troleibusas.islipa(5);
+troleibusas1.islipa(3);
 
-troleibusas.vaziuoja();
+troleibusas1.vaziuoja(); // 2
 
-troleibusas.islipa(3);
+troleibusas2.ilipa(10);
 
-troleibusas.vaziuoja();
+troleibusas2.vaziuoja(); // 10
 
-troleibusas.islipa(3);
+troleibusas2.islipa(7);
 
-troleibusas.vaziuoja();
+troleibusas2.vaziuoja(); // 3
+
+Troleibusas.keleiviuSkaiciusVisuoseTroleibusuose(); // 5
