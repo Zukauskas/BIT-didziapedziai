75798cb7d1ce0be84da4f867adbb9e9d26134a4d
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-07 14:26:38 +0200

    classes

--- 005/index.js
+++ 005/index.js
@@ -1,9 +1,71 @@
-// Lesson 5 - Local Storage
+// Lesson 5
 
-// let color = 'skyblue';
+const obj = {};
 
-// localStorage.setItem('color', color);
+const arr = [];
 
-localStorage.removeItem('color');
+const map = new Map();
 
-console.log(localStorage.getItem('color'));
+class SayName {
+    constructor() {
+        this.papers = 'Yes';
+    }
+
+    static voice = 'Big One';
+
+    static whatIsYourVoice() {
+        return this.voice; // 'this' is a class
+    }
+
+    whatIsYourVoiceNotStatic() {
+        return this.constructor.voice;
+        // return this.voice; // 'this' is an object
+    }
+
+    whatIsYourName() {
+        return this.name;
+    }
+}
+
+class Cat extends SayName {
+    constructor(name, color) {
+        super();
+        this.name = name;
+        this.color = color;
+        this.age = Math.floor(Math.random() * 10) + 1;
+    }
+
+    static voice = 'Small One';
+
+    whatIsYourName() {
+        return 'Miau';
+    }
+}
+
+class Dog extends SayName {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.age = Math.floor(Math.random() * 15) + 1;
+        this.papers = 'No';
+    }
+}
+
+const murka = new Cat('Mūrka', 'brown');
+const pilkis = new Dog('Pilkis');
+
+// console.log(obj);
+// console.log(arr);
+// console.log(map);
+// console.log(murka);
+// console.log(pilkis);
+// console.log(murka.whatIsYourName());
+// console.log(pilkis.whatIsYourName());
+// console.log(SayName.voice);
+// console.log(Cat.voice);
+// console.log(Dog.voice);
+// console.log(murka.voice);
+console.log(Cat.whatIsYourVoice());
+console.log(Dog.whatIsYourVoice());
+console.log(murka.whatIsYourVoiceNotStatic());
+console.log(pilkis.whatIsYourVoiceNotStatic());
