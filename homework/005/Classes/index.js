137e940e0889232e466d593342eb9a15464c798d
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-07 23:08:08 +0200

    Classes 5th task

--- homework/005/Classes/index.js
+++ homework/005/Classes/index.js
@@ -38,6 +38,8 @@ kibiriukas.kiekPririnktaAkmenu();
 
 2. Sukurti klasę Pinigine. Konstruktoriuje sukurti dvi savybes popieriniaiPinigai ir metaliniaiPinigai. Parašyti metodą ideti(kiekis), kuris prideda pinigus į piniginę. Jeigu kiekis nedidesnis už 2, tai prideda prie metaliniaiPinigai, jeigu kitaip- prie popieriniaiPinigai. Parašykite metodą skaiciuoti(), kuris suskaičiuotų ir išvestų į konsolę popieriniaiPinigai ir metaliniaiPinigai sumą. Sukurti klasės objektą ir pademonstruoti veikimą. Nesvarbu kokios popierinės kupiūros ir metalinės monetos egzistuoja realiame pasaulyje.
 
+6. Patobulinti 2 uždavinio piniginę taip, kad būtų galima skaičiuoti kiek piniginėje yra monetų ir kiek banknotų. Parašyti metodą monetos(), kuris skaičiuotų kiek yra piniginėje monetų ir metoda banknotai() - popierinių pinigų skaičiavimui. Kiekvieną atskirą dėjimą (ideti(kiekis) metodo kvietimą) laikykite vienu banknotu ar viena moneta.
+
 */
 
 class Pinigine {
@@ -73,9 +75,7 @@ console.clear();
 
 /*
  3. Sukurti klasę Troleibusas. Konstruktoriuje sukurti savybę keleiviuSkaicius kuri yra lygi 0. Parašyti du metodus: ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius). O taip pat parašyti metoda vaziuoja(), kuris į konsolę išvestų troleibusu važiuojančių keleivių skaičių. Atkreipkite dėmesį, kad troleibusu važiuoti neigiamas keleivių skaičius negali.
-*/
 
-/* 
 4. (STATIC) Sukurti metodą keleiviuSkaiciusVisuoseTroleibusuose(), kuris rodytų bendrą keleivių skaičių visuose Troleibusas objektuose. Bendram kelevių skaičiaus skaičiavimui sukurkite statinį metodą bendrasKeleiviuSkaicius(keleiviuSkaicius), kuris pridėtų arba atimtų keleivius iš statinės savybės visiKeleiviai (kurioje yra įrašytas bendras keleivių skaičius). Taip pat atitinkamai modifikuokite metodus ilipa(keleiviuSkaicius) ir islipa(keleiviuSkaicius).
 
 */
@@ -134,3 +134,65 @@ troleibusas2.islipa(7);
 troleibusas2.vaziuoja(); // 3
 
 Troleibusas.keleiviuSkaiciusVisuoseTroleibusuose(); // 5
+
+console.clear();
+
+/* 
+
+5. (MAP) Sukurti klasę PirkiniuKrepselis. Konstruktoriuje sukurti savybę turinys, kuri yra Map tipo objektas. Sukurti tris metodus: idetiSureli(kiekis), idetiPieno(kiekis), idetiDuonos(kiekis). Parašyti metodą krepselioTurinys(), kuris į konsolę išvestų produktų sąrašą (turinys kintamąjį). Pridėti tuos pačius produktus galima po kelis kartus, tokiu atveju produktų kiekis turėtų sumuotis.
+
+*/
+
+class PirkiniuKrepselis {
+    constructor() {
+        this.turinys = new Map();
+    }
+
+    idetiSureli(kiekis) {
+        if (this.turinys.has('Sūreliai')) {
+            this.turinys.set('Sūreliai', this.turinys.get('Sūreliai') + kiekis);
+        } else {
+            this.turinys.set('Sūreliai', kiekis);
+        }
+    }
+
+    idetiPieno(kiekis) {
+        if (this.turinys.has('Pienas')) {
+            this.turinys.set('Pienas', this.turinys.get('Pienas') + kiekis);
+        } else {
+            this.turinys.set('Pienas', kiekis);
+        }
+    }
+
+    idetiDuonos(kiekis) {
+        if (this.turinys.has('Duona')) {
+            this.turinys.set('Duona', this.turinys.get('Duona') + kiekis);
+        } else {
+            this.turinys.set('Duona', kiekis);
+        }
+    }
+
+    krepselioTurinys() {
+        console.log(this.turinys);
+    }
+}
+
+const pirkiniuKrepselis = new PirkiniuKrepselis();
+
+pirkiniuKrepselis.idetiSureli(2);
+
+pirkiniuKrepselis.idetiPieno(1);
+
+pirkiniuKrepselis.idetiDuonos(3);
+
+pirkiniuKrepselis.krepselioTurinys();
+
+pirkiniuKrepselis.idetiSureli(1);
+
+pirkiniuKrepselis.idetiPieno(1);
+
+pirkiniuKrepselis.idetiDuonos(1);
+
+pirkiniuKrepselis.krepselioTurinys();
+
+console.clear();
