3f28a4bc1c172b905819e9cde0e762f7d9f363c3
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-02-08 15:18:18 +0200

    React wip

---
+++ r1/src/Components/006/Cat.jsx
@@ -0,0 +1,5 @@
+function Cat() {
+    return <h3>C A T</h3>;
+}
+
+export default Cat;
---
+++ r1/src/Components/006/Racoon.jsx
@@ -0,0 +1,12 @@
+import Cat from './Cat';
+
+function Racoon() {
+    return (
+        <>
+            <h2>RACOON</h2>
+            <Cat />
+        </>
+    );
+}
+
+export default Racoon;
